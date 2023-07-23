2bb35cb0b8f0a3369c79530ab5575cde184de66f
soumit-s <soumit.srim@gmail.com>
2023-07-24 00:20:52 +0530

    Svg Components with at least width and height as props. Most take colors as well

---
+++ src/svgs/SvgFrontendComponent1.js
@@ -0,0 +1,5 @@
+export default function SvgFrontendComponent1({ width, height, color }) {
+    return (
+        <svg width={width} height={height} display='block' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M3 10V18C3 19.1046 3.89543 20 5 20H9M3 10V6C3 4.89543 3.89543 4 5 4H19C20.1046 4 21 4.89543 21 6V10M3 10H21M21 10V13" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M15 16L13 18L15 20" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M19 16L21 18L19 20" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> <circle cx="6" cy="7" r="1" fill={color}></circle> <circle cx="9" cy="7" r="1" fill={color}></circle> </g></svg>
+    )
+}
---
+++ src/svgs/SvgVueComponent.js
@@ -0,0 +1,5 @@
+export default function SvgVueComponent({width, height}) {
+    return (
+        <svg display='block' viewBox="0 0 32 32" width={width} height={height} xmlns="http://www.w3.org/2000/svg" fill="#000000"><g strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g><path d="M24.4,3.925H30L16,28.075,2,3.925H12.71L16,9.525l3.22-5.6Z" style={{fill:'#41b883'}}></path><path d="M2,3.925l14,24.15L30,3.925H24.4L16,18.415,7.53,3.925Z" style={{fill: '#41b883'}}></path><path d="M7.53,3.925,16,18.485l8.4-14.56H19.22L16,9.525l-3.29-5.6Z" style={{fill: '#35495e'}}></path></g></svg>
+    )
+}
