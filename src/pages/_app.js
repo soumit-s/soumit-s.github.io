eed77cc46aebc35db3031497f6892d0979dd4ca5
soumit-s <soumit.srim@gmail.com>
2023-07-24 21:28:51 +0530

    Removed script tag import for KUTE.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,10 @@
 import Head from 'next/head'
-import Script from 'next/script'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <Head>
-        <Script src="https://cdn.jsdelivr.net/npm/kute.js@2.2.4/dist/kute.min.js" integrity="sha256-ekq8UIBrOC1lgaJmRRV/c50s5Hy/EBKjBD/7nM9dvp0=" crossorigin="anonymous" />
         <meta name='viewport' content='width=device-width,initial-scale=1.0' />
       </Head>
     </>
