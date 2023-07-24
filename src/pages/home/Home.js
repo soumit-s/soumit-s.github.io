fdb1912a968ad989348492d5b0e9ffeab2358f42
soumit-s <soumit.srim@gmail.com>
2023-07-24 21:12:58 +0530

    Home Page component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,12 @@
-
+import styles from '@/styles/home/Home.module.css'
+import ContentPanel from './ContentPanel'
 
 export default function Home() { 
     return (
         <div>
-            Hello World
+            <div className={styles.content}>
+                <ContentPanel />
+            </div>
         </div>
     )
 }
