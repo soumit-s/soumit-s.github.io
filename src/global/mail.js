8af5f19ca7eb1ee8982902bd479178bda1b4acce
soumit-s <soumit.srim@gmail.com>
2023-07-26 20:09:17 +0530

    Added sendMail function used in the contact me section

---
+++ src/global/mail.js
@@ -0,0 +1,27 @@
+
+
+const TOKEN = '1df714f3-22db-4a3c-8154-29c0d9c2573b'
+
+function generateBody(email, message) {
+	return `EMAIL: ${email},\n
+------------------\n
+${message}
+`
+}
+
+export function sendMail({email, message}, callback) {
+
+	const body=generateBody(email, message)
+
+	console.log(body)
+
+	Email.send({
+		SecureToken: TOKEN,
+		To: 'soumit.srim@gmail.com',
+		From: 'soumit.srim@gmail.com',
+		Subject: "Feedback[from: soumit-s.github.io]",
+		Body: body
+	}).then(
+		message => callback ? callback(message) : null
+	);
+}
