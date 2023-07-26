34450b9e288ddda3177f681f9829259bbf428ce0
soumit-s <soumit.srim@gmail.com>
2023-07-26 20:15:29 +0530

    Added script tag for smtpjs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
+import Script from 'next/script'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
+      <Script src="https://smtpjs.com/v3/smtp.js" />
       <Head>
         <meta name='viewport' content='width=device-width,initial-scale=1.0' />
       </Head>
