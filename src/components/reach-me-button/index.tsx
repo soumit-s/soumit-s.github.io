317230bf95bf224da31d1978478eebcdacc74491
soumit-s <soumit.srim@gmail.com>
2024-04-25 20:52:39 +0530

    Added 'Reach Me' toast fade-in and fade-out animation

--- src/components/reach-me-button/index.tsx
+++ src/components/reach-me-button/index.tsx
@@ -1,35 +1,55 @@
 "use client";
-import SvgMail from "@/components/svg/Mail2.svg";
 import { useState } from "react";
 import { FiMail } from "react-icons/fi";
+import { useCopyToClipboard } from "usehooks-ts";
+import { AnimatePresence, motion } from "framer-motion";
 
-type State = "normal" | "cooldown";
+type State = "normal" | "copying" | "cooldown" | "failed_cooldown";
 
 const ReachMeButton = () => {
   const [state, setState] = useState<State>("normal");
+  const [copiedText, copy] = useCopyToClipboard();
+
+  const handleCopy = async (v: string) => {
+    // initiate copy only if the state is 'normal'
+    if (state !== "normal") return;
+    const success = await copy(v);
+    setState(success ? "cooldown" : "failed_cooldown");
+    setTimeout(() => setState("normal"), 2000);
+  };
+
   return (
     <>
       <button
         className="px-5 py-3 text-neutral-300 bg-black rounded-md font-victor-mono italic text-[0.85em] font-black flex gap-3 items-center shadow-md"
-        onClick={() => {
-          if (state === "normal") {
-            setState("cooldown");
-            setTimeout(() => {
-              setState("normal");
-            }, 2000);
-          }
-        }}
+        onClick={() => handleCopy("hi@soumit.me")}
       >
         Reach Me
         <FiMail className="w-4 h-4" />
       </button>
-      {state === "cooldown" && (
-        <div className="fixed bottom-4 left-0 right-0 z-50">
-          <div className="m-auto w-fit bg-black text-white rounded-lg px-4 py-3 shadow-neutral-400/20 shadow-lg border border-neutral-600 font-reddit-mono text-sm">
-            Email Copied 😊
+      <AnimatePresence>
+        {state === "cooldown" && (
+          <div className="fixed bottom-4 left-0 right-0 z-50">
+            <motion.div
+              className="m-auto w-fit bg-black text-white rounded-lg px-4 py-3 shadow-neutral-400/20 shadow-lg border border-neutral-600 font-reddit-mono text-sm"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              {state === "cooldown" ? (
+                "Email Copied 😊"
+              ) : (
+                <div className="text-center">
+                  Copy Failed 😞
+                  <br />
+                  Here is the email <br />
+                  <u className="underline-offset-4">hi@soumit.me</u>
+                </div>
+              )}
+            </motion.div>
           </div>
-        </div>
-      )}
+        )}
+      </AnimatePresence>
     </>
   );
 };
