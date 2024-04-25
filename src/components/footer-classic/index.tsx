05c8b5dad298d886f71ccd17412fa5863e967f97
soumit-s <soumit.srim@gmail.com>
2024-04-25 19:18:26 +0530

    Animated footer section

--- src/components/footer-classic/index.tsx
+++ src/components/footer-classic/index.tsx
@@ -1,8 +1,8 @@
-import { TiHeart as SvgHeart } from "react-icons/ti";
+import { Variants, motion } from "framer-motion";
 
+import { TiHeart as SvgHeart } from "react-icons/ti";
 import SvgUnsplash from "@/components/svg/Unsplash1.svg";
 import SvgGithub from "@/components/svg/Github2.svg";
-import SvgLinkedin from "@/components/svg/LinkedIn4.svg";
 import SvgSteam from "@/components/svg/Steam1.svg";
 import SvgDiscord from "@/components/svg/Discord1.svg";
 import Link from "next/link";
@@ -13,34 +13,50 @@ const socialsInfo: Array<SocialInfo> = [
   { id: 0, label: "Discord", Icon: SvgDiscord },
   { id: 1, label: "Steam", Icon: SvgSteam },
   { id: 2, label: "Github", Icon: SvgGithub },
-  { id: 3, label: "Unsplash", Icon: SvgUnsplash }
+  { id: 3, label: "Unsplash", Icon: SvgUnsplash },
 ];
 
+const socialsVariants: Variants = {
+  hidden: { scale: 0 },
+  show: { scale: 1 },
+};
+
 const FooterClassic = () => (
-  <footer className="py-8 flex gap-8 items-center flex-row-reverse">
-    <div className="shrink-0 font-cedarville-cursive font-medium text-xl text-neutral-500">Soumit Srimany</div>
+  <footer className="py-8 flex gap-8 items-center">
+    <motion.div
+      className="py-4 font-bricolage-grotesque text-neutral-500 font-extralight flex items-center gap-2 shrink-0"
+      initial={{ opacity: 0, translateX: -20 }}
+      whileInView={{ opacity: 1, translateX: 0 }}
+    >
+      Made with <SvgHeart className="w-6 h-6" /> by{" "}
+      <b className="font-black">me</b>
+    </motion.div>
     <hr className="w-full border-neutral-300" />
-    <div className="flex gap-4 items-center">
+    <motion.div
+      className="flex gap-4 items-center"
+      transition={{ staggerChildren: 0.1 }}
+      initial="hidden"
+      whileInView="show"
+    >
       {socialsInfo.map((s) => (
-        <Link
+        <motion.a
           key={s.id}
           href={s.href ?? ""}
-          className="p-2 border border-neutral-400 rounded-full "
+          className="p-2 border border-neutral-300 rounded-full inline-block"
+          variants={socialsVariants}
         >
           <s.Icon className="w-6 h-6 fill-black" />
-        </Link>
+        </motion.a>
       ))}
-    </div>
+    </motion.div>
     <hr className="w-full border-neutral-300" />
-    {/* <SinCurve
-      length={10}
-      amplitude={7}
-      lineWidth={3}
-      className="w-full"
-    /> */}
-    <div className="py-4 font-bricolage-grotesque text-neutral-500 font-extralight flex items-center gap-2 shrink-0">
-      Made with <SvgHeart className="w-6 h-6" /> by <b className="font-black">me</b>
-    </div>
+    <motion.div
+      className="shrink-0 font-cedarville-cursive font-medium text-xl text-neutral-500"
+      initial={{ opacity: 0, translateX: 20 }}
+      whileInView={{ opacity: 1, translateX: 0 }}
+    >
+      Soumit Srimany
+    </motion.div>
   </footer>
 );
 
